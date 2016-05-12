2853d85b99a940955f11a1bcf5e8eaf18240f261
Nathan Lim <Nathan Lim>
2016-05-12 13:36:09 -0400

    before branch

--- Arduino9DOF/Arduino9DOF.ino/Arduino9DOF.ino.ino
+++ Arduino9DOF/Arduino9DOF.ino/Arduino9DOF.ino.ino
@@ -1,3 +1,10 @@
+/*
+ * Arduino code for communicating with the LSM9DS1 9DOF sensor.
+ * It continuously outputs the roll, pitch, and yaw. These euler angles are calculated from gyroscope + accel + magn, and a complemantary filter is used.
+ * 
+ * Author: Nathan Lim
+ */
+
 #include <SparkFunLSM9DS1.h>
 #include <Wire.h>
 
@@ -14,7 +21,9 @@ float gpitch,groll,gyaw;
 float goffX,goffY,goffZ;
 float tx,ty,tz;
 
-
+/*
+ * gyroscope data is very unstable, we find the average of a number of samples to smooth the data
+ */
 void averageGyro(float *gxT, float *gyT, float *gzT, int num){
   double a=0,b=0,c=0;
   for(int i=0; i<num;i++){
@@ -46,6 +55,7 @@ void setup() {
   gyaw = 0;
   groll = 0;
 
+  //the gyroscope offsets must be first taken into account. 1000 is a lot, but the z axis for the gyropscope may drift if this number goes down.
   averageGyro(&goffX,&goffY,&goffZ,1000);
 
 }
@@ -57,12 +67,16 @@ void loop() {
   imu.readMag();
   averageGyro(&tx,&ty,&tz,10);
 
+  //integrate gyropscope data to find approx angle. This will have errors over time.
+  //If the gyroscope data is very small then it is ignored.
   groll += abs(tx-goffX)<0.05? 0:(tx-goffX)/2000;
   gpitch += abs(ty-goffY)<0.05? 0:(ty-goffY)/2000;
   gyaw += abs(tz-goffZ)<0.025? 0:(tz-goffZ)/2000;
 
+  //Complementary filter. Combined the gyropscope data with the accelerometer and magnetometer data.
   gpitch = gpitch*0.98 - 0.02*atan2(-imu.calcAccel(imu.ax), sqrt(imu.calcAccel(imu.ay) * imu.calcAccel(imu.ay) + imu.calcAccel(imu.az) * imu.calcAccel(imu.az)));
   groll = groll*0.98 - 0.02*atan2(imu.calcAccel(imu.ay), imu.calcAccel(imu.az));
+  //the mx and my must be offset by a certain amount. 0.105 and 0.155 are found by looking at the max and min data and finding where 0,0 should be.
   gyaw = gyaw*0.98 + 0.02*atan2(imu.calcMag(imu.mx)-0.105,imu.calcMag(imu.my)-0.155);
 
 //  Serial.print(groll*180/PI);
