e2c74a11d4d8a9edb27cadd5b2b2770eff334ef8
Nathan Lim <Nathan Lim>
2016-05-12 17:35:53 -0400

    mag compensation

--- Arduino9DOF/Arduino9DOF.ino/Arduino9DOF.ino.ino
+++ Arduino9DOF/Arduino9DOF.ino/Arduino9DOF.ino.ino
@@ -20,6 +20,7 @@ LSM9DS1 imu;
 float gpitch,groll,gyaw;
 float goffX,goffY,goffZ;
 float tx,ty,tz;
+float mxmin,mxmax,mymin,mymax;
 
 /*
  * gyroscope data is very unstable, we find the average of a number of samples to smooth the data
@@ -54,6 +55,12 @@ void setup() {
   gpitch = 0;
   gyaw = 0;
   groll = 0;
+  mxmin = 1.0;
+  mymin = 1.0;
+  mxmax = -1.0;
+  mymax = -1.0;
+
+//  imu.calibrateMag(true);
 
   //the gyroscope offsets must be first taken into account. 1000 is a lot, but the z axis for the gyropscope may drift if this number goes down.
   averageGyro(&goffX,&goffY,&goffZ,1000);
@@ -63,12 +70,27 @@ void setup() {
 
 void loop() {
 
+  if(Serial.available() > 0){
+    String s = Serial.readString();
+    if(s.equals("cal")){
+      imu.magOffset(X_AXIS,(mxmax + mxmin)/2);
+      imu.magOffset(Y_AXIS,(mymax + mymin)/2);
+      Serial.println("Calibrated");
+      mxmin = 1.0;
+      mymin = 1.0;
+      mxmax = -1.0;
+      mymax = -1.0;
+    }
+  }
+
   imu.readAccel();
-  imu.readMag();
+  fixMagOffsets();
+  
   averageGyro(&tx,&ty,&tz,10);
 
+  float m[] = {-(imu.calcMag(imu.my) - ((mymax-mymin)/2 -mymin)),-(imu.calcMag(imu.mx)-((mxmax-mxmin)/2 -mxmin)),imu.calcMag(imu.mz)-0.0417};
+
   //integrate gyropscope data to find approx angle. This will have errors over time.
-  //If the gyroscope data is very small then it is ignored.
   groll += abs(tx-goffX)<0.05? 0:(tx-goffX)/2000;
   gpitch += abs(ty-goffY)<0.05? 0:(ty-goffY)/2000;
   gyaw += abs(tz-goffZ)<0.025? 0:(tz-goffZ)/2000;
@@ -77,12 +99,23 @@ void loop() {
   gpitch = gpitch*0.98 - 0.02*atan2(-imu.calcAccel(imu.ax), sqrt(imu.calcAccel(imu.ay) * imu.calcAccel(imu.ay) + imu.calcAccel(imu.az) * imu.calcAccel(imu.az)));
   groll = groll*0.98 - 0.02*atan2(imu.calcAccel(imu.ay), imu.calcAccel(imu.az));
   //the mx and my must be offset by a certain amount. 0.105 and 0.155 are found by looking at the max and min data and finding where 0,0 should be.
-  gyaw = gyaw*0.98 + 0.02*atan2(imu.calcMag(imu.mx)-0.105,imu.calcMag(imu.my)-0.155);
+//  gyaw = gyaw*0.98 + 0.02*atan2(imu.calcMag(imu.mx)-0.105,imu.calcMag(imu.my)-0.155);
+
+  float xh = m[0]*cos(gpitch) + m[1]*sin(gpitch)*sin(groll) + m[2]*sin(gpitch)*cos(groll);
+  float yh = m[2]*sin(groll)-m[1]*cos(groll);
+
+  gyaw = atan2(-m[1],m[0]);
 
 //  Serial.print(groll*180/PI);
 //  Serial.print(" " + String(gpitch*180/PI));
 //  Serial.println(" " + String((gyaw)*180/PI-DECLINATION));
 
+//  Serial.print(m[1],5);
+//  Serial.print(" ");
+//  Serial.print(m[0],5);
+//  
+//  Serial.print(" ");
+//  Serial.println(((mxmax-mxmin)/2 -mxmin),5);
   Serial.print(groll*180/PI);
   Serial.print(" ");
   Serial.print(gpitch*180/PI);
@@ -93,3 +126,27 @@ void loop() {
   delay(PRINT_SPEED);
 
 }
+
+void fixMagOffsets(){
+  imu.readMag();
+
+  float tempmx = imu.calcMag(imu.mx);
+  float tempmy = imu.calcMag(imu.my);
+
+  //some sort of error
+  if(abs(tempmx) > 0.13 || abs(tempmy) > 0.13)
+    return;
+  
+  if(tempmx > mxmax)
+    mxmax = tempmx;
+  else if(tempmx < mxmin)
+    mxmin = tempmx;
+
+  if(tempmy > mymax)
+    mymax = tempmy;
+  else if(tempmy < mymin)
+    mymin = tempmy;
+  
+}
+
+
